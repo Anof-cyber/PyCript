3e8d79170bc91b8cb7716a4a294058b7903cd991
Sourav Kalal <kalalsourav18@gmail.com>
2024-12-17 18:53:58 +0530

    fixed error in string crypto, fixed path error in windows, fixed os unlink error

--- pycript/execution.py
+++ pycript/execution.py
@@ -2,28 +2,24 @@ import subprocess
 from .gui import logerrors
 import tempfile
 from os import remove
-from .temp_file import parse_temp_file_output
+from .temp_file import parse_temp_file_output, create_temp_file
 
 def execute_command(selectedlang, path, data, headervalue=None):
 
     try:
-        temp_file_path = tempfile.NamedTemporaryFile(delete=False).name
-        with open(temp_file_path, "wb") as file:
-            file.write(bytes(data))  # Write the byte array directly to the file
-            file.write(b'\n--BODY_END--\n')  # Write the binary body end marker
-            if headervalue is not None:
-                file.write(headervalue.encode('utf-8'))
+        temp_file_path = create_temp_file(data,headervalue)
             #temp_file_path = temp_file.name
-        
         command = []
         if selectedlang:
-            command.append('"' + selectedlang + '"')
+            command.append(selectedlang)  # Add the selected language executable directly
 
         if path.endswith(".jar"):
             command.extend(["-jar"])
 
-        command.extend(['"' + path + '"',"-d", temp_file_path])
-        command_str = ' '.join(command)
+        command.extend([path, "-d", temp_file_path])
+
+        # Log the command for debugging
+        command_str = ' '.join('"{0}"'.format(arg) if ' ' in arg else arg for arg in command)
         logerrors("$ " + command_str)
 
         process = subprocess.Popen(
@@ -43,11 +39,13 @@ def execute_command(selectedlang, path, data, headervalue=None):
             logerrors(output.strip())
             body, header = parse_temp_file_output(data,headervalue,temp_file_path)
             remove(temp_file_path)
+            body = "g"
+            header = 'h'
             if body:
                 return body, header  
             else:
                 return False
     except Exception as e:
         logerrors(str(e))
-        remove(temp_file_path)
+        #remove(temp_file_path)
         return False
--- pycript/temp_file.py
+++ pycript/temp_file.py
@@ -1,8 +1,14 @@
+import os
+import tempfile
+import random
+import string
+
+user_home = os.path.expanduser("~")
+pycript_dir = os.path.join(user_home, ".pycript")
 
 def parse_temp_file_output(original_data, original_header, temp_file_path):
     with open(temp_file_path, 'rb') as temp_file:
         file_content = temp_file.read()
-
     body_end_marker = b'\n--BODY_END--\n'
 
     # Split the file content using the marker
@@ -16,5 +22,27 @@ def parse_temp_file_output(original_data, original_header, temp_file_path):
         header_data = parts[1].strip()
     else:
         header_data = original_header
-
     return body_data, header_data
+
+
+def create_temp_file(data, headervalue=None):
+    # Get the temp directory path
+    random_file_name = ''.join([random.choice(string.ascii_letters + string.digits) for _ in range(12)])
+    file_path = os.path.join(pycript_dir, random_file_name)
+    
+    # Write data to the file
+    with open(file_path, "wb") as file:
+        file.write(bytes(data))  # Write the byte array directly to the file
+        file.write(b'\n--BODY_END--\n')  # Write the binary body end marker
+        if headervalue is not None:
+            file.write(headervalue.encode('utf-8'))
+    
+    return file_path
+
+
+def create_temp_dir():
+    
+    if not os.path.exists(pycript_dir):
+        os.makedirs(pycript_dir)
+    temp_dir = tempfile.mkdtemp(dir=pycript_dir)
+    
