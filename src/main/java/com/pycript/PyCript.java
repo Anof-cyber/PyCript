c39eb2b15a7e7196169e67f980e559d22e947497
Sourav Kalal <kalalsourav18@gmail.com>
2025-11-21 19:23:45 +0530

    WIP Context menu Decrypt Request

--- src/main/java/com/pycript/PyCript.java
+++ src/main/java/com/pycript/PyCript.java
@@ -4,12 +4,20 @@ import burp.api.montoya.BurpExtension;
 import burp.api.montoya.MontoyaApi;
 import burp.api.montoya.logging.Logging;
 import burp.api.montoya.ui.UserInterface;
+import burp.api.montoya.ui.contextmenu.ContextMenuEvent;
+import burp.api.montoya.ui.contextmenu.ContextMenuItemsProvider;
+import burp.api.montoya.ui.contextmenu.MessageEditorHttpRequestResponse;
+import burp.api.montoya.http.message.requests.HttpRequest;
 import com.pycript.ui.ConfigTab;
 import com.pycript.ui.DecryptedRequestTab;
 import com.pycript.ui.LogTab;
+import com.pycript.EncDec.Request;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.Component;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PyCript implements BurpExtension
 
@@ -29,6 +37,60 @@ public class PyCript implements BurpExtension
         api.userInterface().registerSuiteTab("PyCript", new PyCriptTab(api));
         api.userInterface().registerHttpRequestEditorProvider(new PyCriptRequestTab(api));
         api.userInterface().registerHttpResponseEditorProvider(new PyCriptResponseTab(api));
+        api.userInterface().registerContextMenuItemsProvider(new PyCriptContextMenu(api));
+    }
+
+    private static class PyCriptContextMenu implements ContextMenuItemsProvider
+    {
+        private final MontoyaApi api;
+
+        public PyCriptContextMenu(MontoyaApi api)
+        {
+            this.api = api;
+        }
+
+        @Override
+        public List<Component> provideMenuItems(ContextMenuEvent event)
+        {
+            List<Component> menuItems = new ArrayList<>();
+
+            if (event.messageEditorRequestResponse().isPresent())
+            {
+                MessageEditorHttpRequestResponse messageEditor = event.messageEditorRequestResponse().get();
+
+                JMenuItem decryptRequestItem = new JMenuItem("Decrypt Request");
+                decryptRequestItem.addActionListener(e -> decryptRequestFromMenu(messageEditor));
+                menuItems.add(decryptRequestItem);
+            }
+
+            return menuItems;
+        }
+
+        private void decryptRequestFromMenu(MessageEditorHttpRequestResponse messageEditor)
+        {
+            if (ConfigTab.selectedRequestType == null || ConfigTab.selectedRequestType.equals("None"))
+            {
+                api.logging().logToOutput("Request decryption is not configured");
+                return;
+            }
+
+            HttpRequest originalRequest = messageEditor.requestResponse().request();
+            if (originalRequest == null)
+            {
+                api.logging().logToOutput("No request found");
+                return;
+            }
+
+            try
+            {
+                HttpRequest decryptedRequest = Request.decrypt(originalRequest, api);
+                api.logging().logToOutput(decryptedRequest.toString());
+            }
+            catch (Exception ex)
+            {
+                api.logging().logToError("Error decrypting request: " + ex.getMessage());
+            }
+        }
     }
 
     private static class PyCriptTab extends JPanel
